f43ee2995fd79bc151c68587064f0040d83ea496
Eddie Dinkz <codemaster.eddie@gmail.com>
2016-02-07 16:51:58 +0530

    Remove conditions to add NULL in comline[] and codline[]

--- processfiles.c
+++ processfiles.c
@@ -14,7 +14,7 @@
 // it also write code lines and comments lines to the code and comment files
 void processfiles(FILE *fsrc, FILE *fcod, FILE *fcom)
 {
-    // initialize rawline, codline, comline array (initialized to an emmpty string for safety)
+    // initialize rawline, codline, comline array (initialized to an empty string for safety)
     char rawline[MAXRAWCHAR] = "", codline[MAXCODCHAR] = "", comline[MAXCOMCHAR] = "";
     // pointers to handle above arrays
     char *rp, *cp, *cop;
@@ -28,7 +28,7 @@ void processfiles(FILE *fsrc, FILE *fcod, FILE *fcom)
         n_rawlines++;   // increment number of rawlines processed so far.
         
         if (strlen(rawline) > 1) { // process if rawline is not a one character
-                                   // this prevents proccessing empty lines
+                                   // this prevents processing empty lines
             
             if (*rp != ';')        // if the string not begins with a ';' it's a code line
                 n_codelines++;     // so increment the number of codelines processed so far
@@ -38,8 +38,8 @@ void processfiles(FILE *fsrc, FILE *fcod, FILE *fcom)
                 *cp++ = *rp++; // copy character from rp to cp then increment cp and rp
                 *cop++ = ' ';  // add a spaces in comline[] for each character copied into codline[] 
             }
-            
-            //if (*rp == ';' || *rp == '\n' || *rp == '\0')
+
+
             // terminate the codline string
             *cp = '\0';
             
@@ -48,17 +48,18 @@ void processfiles(FILE *fsrc, FILE *fcod, FILE *fcom)
             
             // if a comment found 
             if (*rp == ';') {
+
                 // increment n_commlines (number of comment lines processed so far)
                 n_commlines++;
-                // while a LF or NULL char occurs copy rawline[] to commline[]
+
+                // while a LF or NULL char occurs copy rawline[] to comline[]
                 while (*rp != '\n' && *rp)
                     *cop++ = *rp++;
 
-                //if (*rp == '\n' || *rp == '\0')
-                // terminate the commline[] string
+                // terminate the comline[] string
                 *cop = '\0';
                 
-                // print the commline[] string on fcomm strin
+                // print the comline[] string on fcom string
                 fprintf(fcom, "%s\n", comline);
             }
         }
